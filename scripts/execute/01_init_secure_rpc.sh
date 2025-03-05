1eabab01a2ef26b37d4dce3a7b4f8d3a5123cb16
Gylman <76925587+gylman@users.noreply.github.com>
2025-03-05 20:41:46 +0900

    Update 01_init_secure_rpc.sh

--- scripts/execute/01_init_secure_rpc.sh
+++ scripts/execute/01_init_secure_rpc.sh
@@ -16,6 +16,6 @@ sed -i.temp "s|external_rpc_url = \"http://127.0.0.1:9000\"|external_rpc_url = \
 sed -i.temp "s|sequencer_rpc_url_list = \"http://127.0.0.1:3000\"|sequencer_rpc_url_list = \"$SEQUENCER_RPC_URL_LIST\"|g" $CONFIG_FILE_PATH
 
 sed -i.temp "s|encrypted_transaction_type = \"skde\"|encrypted_transaction_type = \"$ENCRYPTED_TRANSACTION_TYPE\"|g" $CONFIG_FILE_PATH
-sed -i.temp "s|distributed_key_generation_rpc_url = \"http://127.0.0.1:7100\"|distributed_key_generation_rpc_url = \"$DISTRIBUTED_KEY_GENERATOR_RPC_URL\"|g" $CONFIG_FILE_PATH
+sed -i.temp "s|distributed_key_generation_rpc_url = \"http://127.0.0.1:7100\"|distributed_key_generation_rpc_url = \"$KEY_GENERATOR_EXTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
 
 rm $CONFIG_FILE_PATH.temp
