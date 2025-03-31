7a768277c71e871354e1f46ba40817c9841a3f51
stompesi <stompesi@gmail.com>
2025-03-31 07:47:42 +0000

    Change error (using thiserror)

--- src/error.rs
+++ src/error.rs
@@ -1,65 +1,92 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
-    Syscall(std::io::Error),
-    Config(crate::types::config::ConfigError),
-    RpcServer(radius_sdk::json_rpc::server::RpcServerError),
+    #[error("System IO error: {0}")]
+    Syscall(#[from] std::io::Error),
+
+    #[error("Config error: {0}")]
+    Config(#[from] crate::types::config::ConfigError),
+
+    #[error("RPC server error: {0}")]
+    RpcServer(#[from] radius_sdk::json_rpc::server::RpcServerError),
+
+    #[error("Uninitialized state")]
     Uninitialized,
+
+    #[error("Failed to fetch response")]
     FetchResponse,
+
+    #[error("Invalid tx-orderer port")]
     InvalidTxOrdererPort,
+
+    #[error("Invalid secure RPC port")]
     InvalidSecureRpcPort,
 
+    #[error("Raw transaction list is empty")]
     EmptyRawTransaction,
+
+    #[error("TxOrderer RPC URL is empty")]
     EmptyTxOrdererRpcUrl,
 
-    DistributedKeyGenerationClient(radius_sdk::json_rpc::client::RpcClientError),
+    #[error("Distributed Key Generation error: {0}")]
+    DistributedKeyGenerationClient(#[from] radius_sdk::json_rpc::client::RpcClientError),
+
+    #[error("Failed to load config option")]
     LoadConfigOption,
+
+    #[error("Failed to parse TOML config string")]
     ParseTomlString,
+
+    #[error("Failed to remove config directory")]
     RemoveConfigDirectory,
+
+    #[error("Failed to create config directory")]
     CreateConfigDirectory,
+
+    #[error("Failed to create config file")]
     CreateConfigFile,
 
-    // Context
+    // Context-related
+    #[error("Failed to update context")]
     ContextUpdateFail,
+
+    #[error("Requested key does not exist")]
     KeyDoesNotExist,
+
+    #[error("Failed to downcast dynamic type")]
     Downcast,
+
+    #[error("Unexpected None value")]
     NoneType,
 
+    #[error("Failed to get SKDE parameters")]
     FailedToGetSkdeParams,
 
+    #[error("Decryption failed: {0}")]
+    DecryptionError(#[from] skde::delay_encryption::DecryptionError),
+
+    #[error("Failed to encrypt transaction: {0}")]
+    EncryptionError(#[from] skde::delay_encryption::EncryptionError),
+
+    #[error("Failed to decode encrypted payload")]
     DecodeFailed,
-    PvdeZkpInvalid,
-    DecryptionError(skde::delay_encryption::DecryptionError),
 
+    #[error("Encryption not enabled")]
     EncryptionNotEnabled,
+
+    #[error("Unsupported encryption type")]
     UnsupportedEncryptionType,
+
+    #[error("Unsupported decryption type")]
     UnsupportedDecryptionType,
+
+    #[error("Unsupported transaction type")]
     UnsupportedTransactionType,
-}
 
-unsafe impl Send for Error {}
+    #[error("Failed to intialize distributed key generation client")]
+    DistributedKeyGenerationClientNotInitialized,
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for Error {}
-
-impl From<std::io::Error> for Error {
-    fn from(value: std::io::Error) -> Self {
-        Self::Syscall(value)
-    }
-}
-
-impl From<crate::types::config::ConfigError> for Error {
-    fn from(value: crate::types::config::ConfigError) -> Self {
-        Self::Config(value)
-    }
-}
-
-impl From<radius_sdk::json_rpc::server::RpcServerError> for Error {
-    fn from(value: radius_sdk::json_rpc::server::RpcServerError) -> Self {
-        Self::RpcServer(value)
-    }
+    #[error("Serialization error")]
+    SerializationError,
 }
