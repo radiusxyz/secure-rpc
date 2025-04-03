9489dd6074a2e1a55fcf3fbdf10d2c20a44df4e6
stompesi <stompesi@gmail.com>
2025-04-03 10:42:22 +0000

    Update 01_init_secure_rpc.sh

--- scripts/execute/01_init_secure_rpc.sh
+++ scripts/execute/01_init_secure_rpc.sh
@@ -13,9 +13,9 @@ sed -i.temp "s|rollup_rpc_url = \"http://127.0.0.1:8123\"|rollup_rpc_url = \"$RO
 
 sed -i.temp "s|external_rpc_url = \"http://127.0.0.1:9000\"|external_rpc_url = \"$SECURE_RPC_EXTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
 
-sed -i.temp "s|tx_orderer_rpc_url_list = \"http://127.0.0.1:3000\"|tx_orderer_rpc_url_list = \"$TX_ORDERER_RPC_URL_LIST\"|g" $CONFIG_FILE_PATH
+sed -i.temp "s|tx_orderer_rpc_url_list = \"http://127.0.0.1:3000\"|tx_orderer_rpc_url_list = \"$TX_ORDERER_EXTERNAL_RPC_URL_LIST\"|g" $CONFIG_FILE_PATH
 
 sed -i.temp "s|encrypted_transaction_type = \"skde\"|encrypted_transaction_type = \"$ENCRYPTED_TRANSACTION_TYPE\"|g" $CONFIG_FILE_PATH
-sed -i.temp "s|distributed_key_generation_rpc_url = \"http://127.0.0.1:7100\"|distributed_key_generation_rpc_url = \"$DISTRIBUTED_KEY_GENERATOR_RPC_URL\"|g" $CONFIG_FILE_PATH
+sed -i.temp "s|distributed_key_generation_rpc_url = \"http://127.0.0.1:7100\"|distributed_key_generation_rpc_url = \"$DISTRIBUTED_KEY_GENERATOR_EXTERNAL_RPC_URL\"|g" $CONFIG_FILE_PATH
 
 rm $CONFIG_FILE_PATH.temp
