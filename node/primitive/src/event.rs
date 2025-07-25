206ea11af1c569f41d69c27425fc754140c1aa4f
cocoyoon <socozy7210@gmail.com>
2025-07-25 18:06:47 +0900

    feat(node): add RPC handler event types for worker pattern
    Add RpcHandlerEvent enum to support communication between
    workers in the new architecture

---
+++ node/primitive/src/event.rs
@@ -0,0 +1,12 @@
+use tokio::sync::oneshot::Sender;
+
+/// Type of events that are sent from the RPC worker to the secure RPC worker
+pub enum RpcHandlerEvent {
+    SendTx { raw_tx: Vec<u8>, should_encrypt: bool, sender: Sender<serde_json::Value> },
+}
+
+/// Type of events that are sent from the operator worker to the secure RPC worker
+pub enum OperatorEvent<TS> {
+    UpdateTrustedSetup(TS),
+    UpdateOperatorList(Vec<String>),
+}
