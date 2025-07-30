56b649d53f7875c73b00edc63deed8bd6773222a
cocoyoon <socozy7210@gmail.com>
2025-07-30 15:30:41 +0900

    refactor(worker): update BlockchainOperatorWorker for event-driven architecture
    - Replace complex select_biased! event handling with simple channel-based approach
    - Use Arc<BlockchainService> for shared ownership across async tasks
    - Simplify event subscription logic using blockchain event receiver
    - Improve error handling for blockchain events with proper logging
    - Remove dependency on futures_util select_biased for cleaner async code

--- node/service/src/worker/operator.rs
+++ node/service/src/worker/operator.rs
@@ -1,23 +1,24 @@
-use secure_rpc_primitives::{Context, TrustedSetupFor};
-use secure_rpc_node_primitive::{blockchain::OperatorContract::TrustedSetupParams, service::blockchain::BlockchainService, OperatorEvent};
+use std::sync::Arc;
+use secure_rpc_primitives::{Context, TrustedSetupFor, };
+use secure_rpc_node_primitive::{blockchain::OperatorContract::TrustedSetupParams, service::blockchain::{BlockchainService, BlockchainEvent, BlockchainServiceError}, OperatorEvent};
 use tokio::sync::mpsc;
-use futures_util::{select_biased, StreamExt, future::FutureExt}; 
 
 pub async fn start_blockchain_operator_worker<C: Context>(_context: &C, blockchain_http_rpc_url: String, contract_address: String) -> anyhow::Result<(tokio::task::JoinHandle<()>, mpsc::Receiver<OperatorEvent<TrustedSetupFor<C>>>, TrustedSetupFor<C>, Vec<String>)> 
 where
     TrustedSetupParams: Into<TrustedSetupFor<C>>,
 {
-    let (worker, rx) = BlockchainOperatorWorker::<C>::new(blockchain_http_rpc_url, contract_address);
+    let (mut worker, rx) = BlockchainOperatorWorker::<C>::new(blockchain_http_rpc_url, contract_address);
     let (trusted_setup, committee_list) = worker.init_service().await?;
     let handle = tokio::spawn(async move { worker.run().await });
     Ok((handle, rx, trusted_setup, committee_list))
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 /// Worker that handles certain state stored on-chain
 pub struct BlockchainOperatorWorker<C: Context> {
-    pub blockchain_service: BlockchainService,
-    operator_event_tx: mpsc::Sender<OperatorEvent<TrustedSetupFor<C>>>,
+    pub blockchain_service: Arc<BlockchainService>,
+    pub operator_event_tx: mpsc::Sender<OperatorEvent<TrustedSetupFor<C>>>,
+    pub blockchain_event_rx: mpsc::Receiver<Result<BlockchainEvent, BlockchainServiceError>>,
 }
 
 impl<C: Context> BlockchainOperatorWorker<C> 
@@ -26,49 +27,33 @@ where
 {
     pub fn new(blockchain_http_rpc_url: String, contract_address: String) -> (Self, mpsc::Receiver<OperatorEvent<TrustedSetupFor<C>>>) {
         let (tx, operator_event_rx) = mpsc::channel(1);
-        let blockchain_service = BlockchainService::new(blockchain_http_rpc_url, contract_address);
+        let (blockchain_service, blockchain_event_rx) = BlockchainService::new(blockchain_http_rpc_url, contract_address);
+        let blockchain_service = Arc::new(blockchain_service);
 
-        (Self { blockchain_service, operator_event_tx: tx }, operator_event_rx)
+        (Self { blockchain_service, operator_event_tx: tx, blockchain_event_rx }, operator_event_rx)
     }
 
-    pub async fn subscribe_events(&self) {
-        let mut trusted_setup_activated_stream = self.blockchain_service.contract_instance.TrustedSetupActivated_filter().watch().await.expect("Failed to subscribe to trusted setup activated events").into_stream();
-        let mut committee_activated_stream = self.blockchain_service.contract_instance.CommitteeActivated_filter().watch().await.expect("Failed to subscribe to committee activated events").into_stream();
-
+    pub async fn subscribe_events(&mut self) {
         loop {
-            select_biased! {
-                maybe_res = trusted_setup_activated_stream.next().fuse() => {
-                    match maybe_res {
-                        Some(res) => {
-                            if let Ok(event) = res {
-                                tracing::debug!("Trusted setup activated at block: {:?}", event.0.activationBlock);
-                                if let Ok(trusted_setup) = self.blockchain_service.get_trusted_setup().await {
-                                    self.operator_event_tx.send(OperatorEvent::UpdateTrustedSetup(trusted_setup.into())).await.expect("Failed to send operator event");
-                                }
-                            } else {
-                                // TODO: Handle this error better
-                                tracing::error!("Failed to decode trusted setup activated event: {:?}", res);
-                                continue;
-                            }
+            while let Some(event) = self.blockchain_event_rx.recv().await {
+                match event {
+                    Ok(BlockchainEvent::TrustedSetupActivated) => {
+                        tracing::info!("Trusted setup activated");
+                        let trusted_setup = self.blockchain_service.get_trusted_setup().await.expect("Failed to get trusted setup");
+                        if let Err(e) = self.operator_event_tx.send(OperatorEvent::UpdateTrustedSetup(trusted_setup.into())).await {
+                            tracing::error!("Failed to send trusted setup activated event: {:?}", e);
                         }
-                        _ => continue,
                     }
-                },
-                maybe_res = committee_activated_stream.next().fuse() => {
-                    match maybe_res {
-                        Some(res) => {
-                            if let Ok(event) = res {
-                                tracing::debug!("Committee activated at block: {:?}", event.0.activationBlock);
-                                if let Ok(operator_list) = self.blockchain_service.get_operator_rpc_urls().await {
-                                    self.operator_event_tx.send(OperatorEvent::UpdateOperatorList(operator_list)).await.expect("Failed to send operator event");
-                                }
-                            } else {
-                                // TODO: Handle this error better
-                                tracing::error!("Failed to decode committee activated event: {:?}", res);
-                                continue;
-                            }
+                    Ok(BlockchainEvent::CommitteeActivated) => {
+                        tracing::info!("Committee activated");
+                        let operator_list = self.blockchain_service.get_operator_rpc_urls().await.expect("Failed to get operator RPC URLs");
+                        if let Err(e) = self.operator_event_tx.send(OperatorEvent::UpdateOperatorList(operator_list)).await {
+                            tracing::error!("Failed to send committee activated event: {:?}", e);
                         }
-                        _ => continue,
+                    }
+                    Err(e) => {
+                        tracing::error!("Failed to receive blockchain event: {:?}", e);
+                        continue;
                     }
                 }
             }
@@ -82,7 +67,7 @@ where
     }
 
     /// Simple loop that runs forever
-    pub async fn run(&self) {
+    pub async fn run(&mut self) {
         self.subscribe_events().await
     }
 }
